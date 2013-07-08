b5372ebd704e7b18989796adddd8fe77df836b33
tremolina <michus72@gmail.com>
2013-07-08 14:57:56 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utf8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
